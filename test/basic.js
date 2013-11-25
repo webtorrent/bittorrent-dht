6a6c3b48ebbea8f7af6ef379f7b3693590bf28e7
Feross Aboukhadijeh <feross@feross.org>
2013-11-25 06:29:37 -0800

    add test case (using live dht network)

--- test/basic.js
+++ test/basic.js
@@ -1,6 +1,23 @@
+var DHT = require('../')
+var once = require('once')
 var test = require('tape')
 
-test('TODO', function (t) {
-  t.ok(true)
-  t.end()
+// TODO: Improve reliability by not using live network
+
+test('Find nodes (Pride & Prejudice)', function (t) {
+  t.plan(1)
+
+  var hash = '1E69917FBAA2C767BCA463A96B5572785C6D8A12' // Pride & Prejudice
+  var dht = new DHT(new Buffer(hash, 'hex'))
+  dht.findPeers(300)
+
+  dht.on('node', once(function (peer) {
+    t.pass('Found at least one other DHT node')
+    dht.close()
+  }))
+
+  // TODO
+  // dht.on('peer', once(function (peer) {
+  //   t.pass('Found at least one peer that has the file')
+  // }))
 })
