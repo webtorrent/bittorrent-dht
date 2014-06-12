2df311996d1df27a55b657066aa50c1b6b527bfb
Feross Aboukhadijeh <feross@feross.org>
2014-06-11 22:18:14 -0700

    test lookup using mainline DHT, incl multiple at same time

--- test/lookup.js
+++ test/lookup.js
@@ -3,43 +3,68 @@ var hat = require('hat')
 var portfinder = require('portfinder')
 var test = require('tape')
 
-// function createNetwork (num) {
-//   var dhts = []
-//   for (var i = 0; i < num; i++) {
-//     dhts.push(new DHT())
-//   }
-// }
+var pride = '1E69917FBAA2C767BCA463A96B5572785C6D8A12'.toLowerCase() // Pride & Prejudice
+var leaves = 'D2474E86C95B19B8BCFDB92BC12C9D44667CFA36'.toLowerCase() // Leaves of Grass
 
-test('Find nodes (Pride & Prejudice)', function (t) {
-  t.plan(2)
+test('Default bootstrap server returns at least one node', function (t) {
+  t.plan(1)
 
   var dht = new DHT()
 
   dht.once('node', function (node) {
     t.pass('Found at least one other DHT node')
-    dht.destroy(function () {
-      t.pass('dht destroyed')
+    dht.destroy()
+  })
+})
+
+test('Default bootstrap server returns a peer for one torrent', function (t) {
+  t.plan(4)
+
+
+  var dht = new DHT()
+
+  dht.once('node', function (node) {
+    t.pass('Found at least one other DHT node')
+  })
+
+  dht.on('ready', function () {
+    t.pass('dht ready')
+
+    dht.lookup(pride)
+
+    dht.once('peer', function (peer, infoHash) {
+      t.pass('Found at least one peer that has the file')
+      t.equal(infoHash, pride)
+      dht.destroy()
     })
   })
 })
 
-test('Find nodes (Pride & Prejudice)', function (t) {
-  t.plan(2)
-
-  var infoHash = '1E69917FBAA2C767BCA463A96B5572785C6D8A12' // Pride & Prejudice
+test('Default bootstrap server returns a peer for two torrents (simultaneously)', function (t) {
+  t.plan(3)
 
   var dht = new DHT()
-  setTimeout(function () {
-    // HACK HACK HACK
-    dht.lookup(infoHash)
-  }, 10000)
 
-  dht.once('node', function (node) {
-    t.pass('Found at least one other DHT node')
-  })
+  dht.on('ready', function () {
+    t.pass('dht ready')
 
-  dht.once('peer', function (peer) {
-    t.pass('Found at least one peer that has the file')
-    dht.destroy()
+    dht.lookup(pride)
+    dht.lookup(leaves)
+
+    var prideDone = false
+    var leavesDone = false
+    dht.on('peer', function (peer, infoHash) {
+      if (!prideDone && infoHash === pride) {
+        prideDone = true
+        t.pass('Found at least one peer for Pride & Prejudice')
+      }
+      if (!leavesDone && infoHash === leaves) {
+        leavesDone = true
+        t.pass('Found at least one peer for Leaves of Grass')
+      }
+      if (leavesDone && prideDone) {
+        dht.destroy()
+      }
+    })
   })
 })
