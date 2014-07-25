cdeeeba9381e3ce146ba02531bf6980c02131a3e
Feross Aboukhadijeh <feross@feross.org>
2014-07-25 00:19:13 -0700

    improve clarity of 'ready' test

--- test/events.js
+++ test/events.js
@@ -94,31 +94,31 @@ test('`ready` event fires when bootstrap === false', function (t) {
 })
 
 test('`ready` event fires when there are K nodes', function (t) {
-  t.plan(3)
+  t.plan(4)
 
   // dht1 will simulate an existing node (with a populated routing table)
   var dht1 = new DHT({ bootstrap: false })
   common.failOnWarningOrError(t, dht1)
 
   dht1.on('ready', function () {
-    t.equal(dht1.nodes.count(), DHT.K, '`ready` event fires when K nodes are available')
-  })
+    t.pass('dht1 `ready` event fires because { bootstrap: false }')
 
-  // add K nodes to dht1
-  common.addRandomNodes(dht1, DHT.K)
+    common.addRandomNodes(dht1, DHT.K)
+    t.equal(dht1.nodes.count(), DHT.K, 'dht1 has K nodes')
 
-  dht1.listen(function (port) {
-    t.pass('dht listening on port ' + port)
+    dht1.listen(function (port) {
+      t.pass('dht1 listening on port ' + port)
 
-    // dh2 will get all K nodes from dht1 and should also emit a `ready` event
-    var dht2 = new DHT({ bootstrap: '127.0.0.1:' + port })
-    common.failOnWarningOrError(t, dht2)
+      // dht2 will get all K nodes from dht1 and should also emit a `ready` event
+      var dht2 = new DHT({ bootstrap: '127.0.0.1:' + port })
+      common.failOnWarningOrError(t, dht2)
 
-    dht2.on('ready', function () {
-      t.equal(dht1.nodes.count(), DHT.K, 'dht2 gets K nodes from dht1 and fires `ready`')
+      dht2.on('ready', function () {
+        t.equal(dht1.nodes.count(), DHT.K, 'dht2 gets K nodes from dht1 and fires `ready`')
 
-      dht1.destroy()
-      dht2.destroy()
+        dht1.destroy()
+        dht2.destroy()
+      })
     })
   })
 })
