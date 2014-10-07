d81d6232dd84ffde1e6313207455b5ba9fa37981
Feross Aboukhadijeh <feross@feross.org>
2014-10-07 00:22:43 -0700

    Test to prove that #36 and #37 are not an issue

--- test/persist.js
+++ test/persist.js
@@ -19,5 +19,27 @@ test('persist dht', function (t) {
       dht2.destroy()
     })
   })
+})
 
+// https://github.com/feross/bittorrent-dht/pull/36
+test('bootstrap and listen to custom port', function (t) {
+  t.plan(4)
+
+  var dht = new DHT({ bootstrap: [ '1.2.3.4:1000' ] })
+  common.failOnWarningOrError(t, dht)
+
+  t.ok(!dht.listening)
+  dht.listen(12345)
+  t.ok(!dht.listening)
+
+  // bootstrapping should wait until next tick, so user has a chance to
+  // listen to a custom port
+  dht.on('listening', function () {
+    t.ok(dht.listening)
+    t.equal(dht.port, 12345)
+  })
+
+  dht.on('ready', function () {
+    dht.destroy()
+  })
 })
