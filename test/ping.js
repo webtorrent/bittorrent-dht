de03abf8f879168e1317687ef7b1ed7e1018f18a
Robert Kowalski <rok@kowalski.gd>
2017-12-02 18:23:16 +0100

    add test

--- test/ping.js
+++ test/ping.js
@@ -10,7 +10,7 @@ test('testing', function (t) {
     dht2.on('ready', function () {
       dht2.destroy(function () {
         dht2 = dht({bootstrap: ['127.0.0.1:10000']})
-        dht2.on('ready', ping) 
+        dht2.on('ready', ping)
         dht2.listen(20000)
       })
     })
@@ -20,6 +20,43 @@ test('testing', function (t) {
     function ping () {
       t.same(dht1.nodes.toArray().length, 2, 'have two nodes')
       dht1._pingAll(function () {
+        t.same(dht1.nodes.toArray().length, 1, 'should remove all nodes')
+        done()
+      })
+    }
+
+    function done () {
+      dht1.destroy(function () {
+        dht2.destroy(function () {
+          t.end()
+        })
+      })
+    }
+  })
+})
+
+test('testing clones', function (t) {
+  var dht1 = dht({bootstrap: false})
+  var dht3
+
+  dht1.listen(10000, function () {
+    var dht2 = dht({bootstrap: ['127.0.0.1:10000']})
+
+    dht2.on('ready', function () {
+      dht2.destroy(function () {
+        dht3 = dht({bootstrap: ['127.0.0.1:10000']})
+        dht3.on('ready', ping)
+        dht3.listen(20000)
+      })
+    })
+
+    dht2.listen(20000)
+
+    function ping () {
+      t.same(dht3.nodes.toArray().length, 2, 'has two nodes')
+      t.same(dht1.nodes.toArray().length, 2, 'have two nodes')
+      dht1._pingAll(function () {
+        t.same(dht3.nodes.toArray().length, 1, 'should remove all nodes')
         t.same(dht1.nodes.toArray().length, 1, 'should remove all nodes')
         done()
       })
