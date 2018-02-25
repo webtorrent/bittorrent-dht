6e25db6259fca1af2325e665461f6d87a1c5156d
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2018-02-25 21:01:22 +0200

    Add support for ArrayBufferView (like Uint8Array) in toBuffer() function

--- client.js
+++ client.js
@@ -798,6 +798,7 @@ function toNode (node) {
 
 function toBuffer (str) {
   if (Buffer.isBuffer(str)) return str
+  if (ArrayBuffer.isView(str)) return Buffer.from(str.buffer, str.byteOffset, str.byteLength)
   if (typeof str === 'string') return Buffer.from(str, 'hex')
   throw new Error('Pass a buffer or a string')
 }
