b362413cef04f769e1f386d18ab2b215e47e6926
Jimmy Wärting <jimmy@warting.se>
2018-08-27 15:56:39 +0200

    ES next webtorrent/webtorrent#154

--- test/ping.js
+++ test/ping.js
@@ -1,15 +1,15 @@
 var test = require('tape')
-var dht = require('../')
+var DHT = require('../')
 
 test('ping should clear clones', function (t) {
-  var dht1 = dht({bootstrap: false})
+  var dht1 = new DHT({bootstrap: false})
 
   dht1.listen(10000, function () {
-    var dht2 = dht({bootstrap: ['127.0.0.1:10000']})
+    var dht2 = new DHT({bootstrap: ['127.0.0.1:10000']})
 
     dht2.on('ready', function () {
       dht2.destroy(function () {
-        dht2 = dht({bootstrap: ['127.0.0.1:10000']})
+        dht2 = new DHT({bootstrap: ['127.0.0.1:10000']})
         dht2.on('ready', ping)
         dht2.listen(20000)
       })
@@ -36,15 +36,15 @@ test('ping should clear clones', function (t) {
 })
 
 test('ping should clear with three nodes', function (t) {
-  var dht1 = dht({bootstrap: false})
+  var dht1 = new DHT({bootstrap: false})
   var dht3
 
   dht1.listen(10000, function () {
-    var dht2 = dht({bootstrap: ['127.0.0.1:10000']})
+    var dht2 = new DHT({bootstrap: ['127.0.0.1:10000']})
 
     dht2.on('ready', function () {
       dht2.destroy(function () {
-        dht3 = dht({bootstrap: ['127.0.0.1:10000']})
+        dht3 = new DHT({bootstrap: ['127.0.0.1:10000']})
         dht3.on('ready', ping)
         dht3.listen(20000)
       })
