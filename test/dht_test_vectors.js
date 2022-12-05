232b9fd931c4d6b8749bfcd84b3a37d021a84b86
Cas <6506529+ThaUnknown@users.noreply.github.com>
2022-12-05 12:12:49 +0100

    feat: esm (#263)
    BREAKING CHANGE: ESM only
    
    * refactor: update import/export of source files
    
    use esm import export
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>
    
    * refactor: update common test file
    
    use esm const exports and imports
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>
    
    * chore: update import in tests
    
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>
    
    * chore: update package.json for esm support
    
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>
    
    * chore: update Readme esm
    
    small fix new DHT() now new DHT
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>
    
    * fix: node:crypto use
    
    use valid absolute URL strings
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>
    
    * chore: order module imports
    
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>
    
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>
    Co-authored-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- test/dht_test_vectors.js
+++ test/dht_test_vectors.js
@@ -1,8 +1,8 @@
-const common = require('./common')
-const DHT = require('../')
-const test = require('tape')
-const crypto = require('crypto')
-const ed = require('bittorrent-dht-sodium')
+import crypto from 'node:crypto'
+import ed from 'bittorrent-dht-sodium'
+import test from 'tape'
+import DHT from '../index.js'
+import * as common from './common.js'
 
 // test vectors from http://bittorrent.org/beps/bep_0044.html
 test('dht store test vectors - test 1 (mutable)', t => {
--- test/internal.js
+++ test/internal.js
@@ -1,6 +1,6 @@
-const common = require('./common')
-const DHT = require('../')
-const test = require('tape')
+import test from 'tape'
+import DHT from '../index.js'
+import * as common from './common.js'
 
 test('`ping` query send and response', t => {
   t.plan(2)
